fc2bdee140033325d66ea984c88b6c3282fa2b59
YuriHermont <yuri.hermont1@estudante.ufla.br>
2019-04-16 15:26:15 -0300

    Create classC.cpp

---
+++ classC.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+class C{
+	string C1;
+	int C2;
+	
+	string getC1(){
+		return C1;
+	}
+	int getC2(){
+		return C2;
+	}
+	void setC1(string a){
+		this.C1 = a;
+	}
+	void setC2(int a){
+		this.C2 = a;
+	}
+	void MC1(){
+		cout<<"MC1"<<endl;
+	}
+	void MC2(){
+		cout<<"MC2"<<endl;
+	}
+}
