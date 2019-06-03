959e4892e9010b03cb7c18e50b39cb369a298a9b
YuriHermont1995 <49692400+YuriHermont1995@users.noreply.github.com>
2019-06-03 13:32:32 -0300

    Add files via upload

---
+++ classD.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+class D{
+	int D1;
+	float D2;
+	
+	int getD1(){
+		return D1;
+	}
+	float getD2(){
+		return D2;
+	}
+	void setD1(int a){
+		this.D1 = a;
+	}
+	void setD2(float a){
+		this.D2 = a;
+	}
+	void MD1(){
+		cout<<"MD1"<<endl;
+	}
+	void MD2(){
+		cout<<"MD2"<<endl;
+	}
+}
