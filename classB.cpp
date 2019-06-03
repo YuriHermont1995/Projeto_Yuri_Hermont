5e741df46c2d8d67cfbbf0c159eb90c5c7f47f95
YuriHermont1995 <49692400+YuriHermont1995@users.noreply.github.com>
2019-06-03 13:35:17 -0300

    Atualizaçao B e C
    Atualização das Classes B e C 
    Questao 12

--- classB.cpp
+++ classB.cpp
@@ -21,4 +21,7 @@ class B{
 	void MB2(){
 		cout<<"MB2"<<endl;
 	}
+	void MB3(){
+		cout<<"MB3"<<endl;
+	}
 }
--- classC.cpp
+++ classC.cpp
@@ -21,4 +21,7 @@ class C{
 	void MC2(){
 		cout<<"MC2"<<endl;
 	}
+	void MC3(){
+		cout<<"MC3"<<endl;
+	}
 }
